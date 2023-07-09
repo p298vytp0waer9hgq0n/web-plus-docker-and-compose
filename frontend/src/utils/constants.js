0c1e6603ef540a820c33533a4a90c71813474fee
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-07-10 00:45:45 +0500

    fix: small fixes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.sava.students.nomoredomains.work";
+export const URL = "https://api.sava.students.nomoredomains.work";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
